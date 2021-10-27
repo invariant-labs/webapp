b20ac798459301d6183c1acd961414100e187ce7
PawelSzydlo <pawel.szydlo.99@gmail.com>
2021-10-27 15:44:12 +0200

    fixed storybook

--- src/components/NewDesign/LiquidityItem/LiquidityItem.stories.tsx
+++ src/components/NewDesign/LiquidityItem/LiquidityItem.stories.tsx
@@ -5,24 +5,32 @@ storiesOf('liquidityPosition/items', module)
   .add('itemActive', () => {
     return (
       <LiquidityItem
-        active={true}
-        nameToSwap={'BTC'}
-        nameFromSwap={'SNY'}
-        min={2149.6}
-        max={149.6}
-        fee={0.05}
+        data={{
+          active: true,
+          nameToSwap: 'BTC',
+          nameFromSwap: 'SNY',
+          min: 2149.6,
+          max: 149.6,
+          fee: 0.05
+        }}
+        actionMin={() => console.log('clicked min button')}
+        actionMax={() => console.log('clicked max button')}
       />
     )
   })
   .add('itemClose', () => {
     return (
       <LiquidityItem
-        active={false}
-        nameToSwap={'BTC'}
-        nameFromSwap={'SNY'}
-        min={2149.6}
-        max={149.6}
-        fee={0.05}
+        data={{
+          active: false,
+          nameToSwap: 'BTC',
+          nameFromSwap: 'SNY',
+          min: 2149.6,
+          max: 149.6,
+          fee: 0.05
+        }}
+        actionMin={() => console.log('clicked min button')}
+        actionMax={() => console.log('clicked max button')}
       />
     )
   })
