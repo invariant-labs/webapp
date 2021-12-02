a904dc63440cf418d9667e10ec60954c9aa13ee3
Adrian <adrianwojciak99@gmail.com>
2021-12-02 10:25:30 +0100

    a link to the past

--- src/pages/SinglePositionPage/SinglePositionPage.tsx
+++ src/pages/SinglePositionPage/SinglePositionPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
-import useStyles from './styles'
+import { Grid, Typography } from '@material-ui/core'
 import { SinglePositionWrapper } from '@containers/SinglePositionWrapper/SinglePositionWrapper'
+import { Link } from 'react-router-dom'
+import backIcon from '@static/svg/back-arrow.svg'
+import useStyles from './styles'
 
 export interface IProps {
   id: string
@@ -11,7 +13,18 @@ export const SinglePositionPage: React.FC<IProps> = ({ id }) => {
   const classes = useStyles()
 
   return (
-    <Grid container className={classes.container}>
+    <Grid container className={classes.container} direction='column'>
+      <Link to='/pool' style={{ textDecoration: 'none' }}>
+        <Grid
+          className={classes.back}
+          container
+          item
+          alignItems='center'
+        >
+          <img className={classes.backIcon} src={backIcon} />
+          <Typography className={classes.backText}>Back to Liquidity Positions List</Typography>
+        </Grid>
+      </Link>
       <SinglePositionWrapper id={id} />
     </Grid>
   )
--- src/pages/SinglePositionPage/styles.ts
+++ src/pages/SinglePositionPage/styles.ts
@@ -1,11 +1,34 @@
 import { makeStyles } from '@material-ui/core/styles'
+import { colors, typography } from '@static/theme'
 
 const useStyles = makeStyles(() => ({
   container: {
     display: 'flex',
     justifyContent: 'center',
     backgroundColor: 'transparent',
-    marginTop: 65
+    marginTop: 65,
+    marginInline: 'auto',
+    width: 1058
+  },
+  back: {
+    height: 24,
+    marginBottom: 18,
+    width: 'fit-content',
+    transition: 'filter 300ms',
+
+    '&:hover': {
+      filter: 'brightness(2)'
+    }
+  },
+  backIcon: {
+    width: 22,
+    height: 24,
+    marginRight: 12
+  },
+  backText: {
+    color: colors.invariant.lightInfoText,
+    WebkitPaddingBefore: '2px',
+    ...typography.body2
   }
 }))
 
