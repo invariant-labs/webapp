7e32db6cf2118dda06f18eaf23576ca09936134b
Pawel Krawczyk <pawel.krawczyk@interia.com>
2021-12-28 17:42:52 +0100

    add telegram and medium

--- src/static/links.ts
+++ src/static/links.ts
@@ -2,6 +2,6 @@ export const social = {
   twitter: 'https://twitter.com/invariant_labs',
   github: 'https://github.com/invariant-labs',
   discord: 'https://discord.gg/w6hTeWTJvG',
-  telegram: '',
-  medium: ''
+  telegram: 'https://t.me/+-C9-e6L08AY4YWI0',
+  medium: 'https://medium.com/@invariant_labs'
 }
