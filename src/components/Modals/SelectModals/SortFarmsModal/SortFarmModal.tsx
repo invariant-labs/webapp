36de819246d6371f34307928ad924468e8aeb8a7
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2022-01-06 20:57:58 +0100

    Add sort dropdowns

---
+++ src/components/Modals/SelectModals/SortFarmsModal/SortFarmModal.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { Typography, Popover, Grid, CardMedia, Box, Button } from '@material-ui/core'
+import useStyles from '../style'
+import { SortItem } from '@components/Inputs/SortSelect/SortSelect'
+import CustomScrollbar from '../CustomScrollbar'
+import classNames from 'classnames'
+
+export interface ISelectSortFarmsModal {
+  sortItems: SortItem[]
+  open: boolean
+  handleClose: () => void
+  anchorEl: HTMLButtonElement | null
+  centered?: boolean
+  onSelect: (name: string) => void
+  onlyText: boolean
+}
+
+export const SelectSortFarmsModal: React.FC<ISelectSortFarmsModal> = ({
+  sortItems,
+  open,
+  handleClose,
+  onSelect,
+  onlyText,
+  anchorEl
+}) => {
+  const classes = useStyles()
+  const [value, setValue] = useState<string>('')
+  const buttons =
+    sortItems &&
+    sortItems.map(sortItem => (
+      <Grid
+        container
+        key={sortItem ? `sortItem-${sortItem.name}` : ''}
+        className={onlyText ? classes.filterItemText : classes.filterItem}
+        alignItems='center'
+        wrap='nowrap'
+        onClick={() => {
+          setValue('')
+          handleClose()
+        }}>
+        <Button
+          className={classes.button}
+          color='primary'
+          variant='contained'
+          startIcon={
+            !sortItem ? null : <CardMedia image={sortItem.icon} className={classes.filterIcon} />
+          }
+          disableRipple>
+          <span
+            style={{ whiteSpace: 'nowrap' }}
+            className={onlyText ? classes.filterNameText : classes.filterName}>
+            {sortItem.name}
+          </span>
+        </Button>
+      </Grid>
+    ))
+
+  return (
+    <Popover
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left'
+      }}
+      classes={{ paper: classes.popoverSort }}
+      open={open}
+      onClose={handleClose}>
+      <Box className={onlyText ? classes.filterListText : classes.filterList}>
+        {buttons.length > 3 ? <CustomScrollbar>{buttons}</CustomScrollbar> : buttons}
+      </Box>
+    </Popover>
+  )
+}
+export default SelectSortFarmsModal
