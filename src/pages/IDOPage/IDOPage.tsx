97a761042351017f78c4075105f7b47d1391ea26
MichalBielicki <80898561+MichalBielicki@users.noreply.github.com>
2022-01-07 20:55:58 +0100

    instaled

---
+++ src/pages/IDOPage/IDOPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import WrappedSwap from '@containers/WrappedSwap/WrappedSwap'
+import useStyles from './styles'
+import { Grid } from '@material-ui/core'
+
+export const IDOPage: React.FC = () => {
+  const classes = useStyles()
+
+  return (
+    <Grid container className={classes.container}>
+      <Grid item>
+        <WrappedSwap />
+      </Grid>
+    </Grid>
+  )
+}
+
+export default IDOPage
