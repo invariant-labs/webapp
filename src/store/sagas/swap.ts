33edbc6c3294862167c52d2ff8db4599ffd83d21
Pawel Krawczyk <pawel.krawczyk@interia.com>
2022-01-11 11:55:29 +0100

    fix lint

--- src/store/sagas/swap.ts
+++ src/store/sagas/swap.ts
@@ -15,21 +15,24 @@ import BN from 'bn.js'
 export function* handleSimulate(): Generator {
   try {
     const allPools = yield* select(pools)
-    const {
-      slippage,
-      simulate
-    } = yield* select(swap)
+    const { slippage, simulate } = yield* select(swap)
     const marketProgram = yield* call(getMarketProgram)
-    const swapPool = allPools.find((pool) =>
-      (simulate.fromToken.toString() === pool.tokenX.toString() && simulate.toToken.toString() === pool.tokenY.toString()) ||
-        (simulate.fromToken.toString() === pool.tokenY.toString() && simulate.toToken.toString() === pool.tokenX.toString())
+    const swapPool = allPools.find(
+      pool =>
+        (simulate.fromToken.toString() === pool.tokenX.toString() &&
+          simulate.toToken.toString() === pool.tokenY.toString()) ||
+        (simulate.fromToken.toString() === pool.tokenY.toString() &&
+          simulate.toToken.toString() === pool.tokenX.toString())
     )
 
     if (!swapPool) {
       return
     }
-    const isXtoY = simulate.fromToken.toString() === swapPool.tokenX.toString() && simulate.toToken.toString() === swapPool.tokenY.toString()
-    const tickMap = yield* call([marketProgram, marketProgram.getTickmap],
+    const isXtoY =
+      simulate.fromToken.toString() === swapPool.tokenX.toString() &&
+      simulate.toToken.toString() === swapPool.tokenY.toString()
+    const tickMap = yield* call(
+      [marketProgram, marketProgram.getTickmap],
       new Pair(simulate.fromToken, simulate.toToken, FEE_TIERS[0])
     )
     if (simulate.amount.gt(new BN(0))) {
@@ -45,14 +48,10 @@ export function* handleSimulate(): Generator {
         market: marketProgram
       }
 
-      yield put(
-        swapActions.changePrice(calculateAveragePrice(simulateObject))
-      )
+      yield put(swapActions.changePrice(calculateAveragePrice(simulateObject)))
       // console.log('simulate price: ', calculateAveragePrice(simulateObject).v.toString())
     } else {
-      yield put(
-        swapActions.changePrice({ v: new BN(0) })
-      )
+      yield put(swapActions.changePrice({ v: new BN(0) }))
     }
   } catch (error) {
     console.log(error)
@@ -62,23 +61,24 @@ export function* handleSimulate(): Generator {
 export function* handleSwap(): Generator {
   try {
     const allPools = yield* select(pools)
-    const {
-      slippage,
-      price,
-      simulate
-    } = yield* select(swap)
+    const { slippage, price, simulate } = yield* select(swap)
     console.log('amount', simulate.amount.toString())
     console.log('price', price.v.toString())
-    const swapPool = allPools.find((pool) =>
-      (simulate.fromToken.toString() === pool.tokenX.toString() && simulate.toToken.toString() === pool.tokenY.toString()) ||
-      (simulate.fromToken.toString() === pool.tokenY.toString() && simulate.toToken.toString() === pool.tokenX.toString())
+    const swapPool = allPools.find(
+      pool =>
+        (simulate.fromToken.toString() === pool.tokenX.toString() &&
+          simulate.toToken.toString() === pool.tokenY.toString()) ||
+        (simulate.fromToken.toString() === pool.tokenY.toString() &&
+          simulate.toToken.toString() === pool.tokenX.toString())
     )
 
     if (!swapPool) {
       return
     }
 
-    const isXtoY = simulate.fromToken.toString() === swapPool.tokenX.toString() && simulate.toToken.toString() === swapPool.tokenY.toString()
+    const isXtoY =
+      simulate.fromToken.toString() === swapPool.tokenX.toString() &&
+      simulate.toToken.toString() === swapPool.tokenY.toString()
 
     const wallet = yield* call(getWallet)
 
@@ -97,19 +97,17 @@ export function* handleSwap(): Generator {
     if (toAddress === null) {
       toAddress = yield* call(createAccount, simulate.toToken)
     }
-    const swapTx = yield* call([marketProgram, marketProgram.swapTransaction],
-      {
-        pair: new Pair(simulate.fromToken, simulate.toToken, FEE_TIERS[0]),
-        XtoY: isXtoY,
-        amount: simulate.amount,
-        knownPrice: price,
-        slippage: slippage,
-        accountX: isXtoY ? fromAddress : toAddress,
-        accountY: isXtoY ? toAddress : fromAddress,
-        byAmountIn: true,
-        owner: wallet.publicKey
-      }
-    )
+    const swapTx = yield* call([marketProgram, marketProgram.swapTransaction], {
+      pair: new Pair(simulate.fromToken, simulate.toToken, FEE_TIERS[0]),
+      XtoY: isXtoY,
+      amount: simulate.amount,
+      knownPrice: price,
+      slippage: slippage,
+      accountX: isXtoY ? fromAddress : toAddress,
+      accountY: isXtoY ? toAddress : fromAddress,
+      byAmountIn: true,
+      owner: wallet.publicKey
+    })
     const connection = yield* call(getConnection)
     const blockhash = yield* call([connection, connection.getRecentBlockhash])
     swapTx.recentBlockhash = blockhash.blockhash
