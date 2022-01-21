db81c32e8bc37ead9381d9ed7da2d0cc19b821fa
Adrian <adrianwojciak99@gmail.com>
2022-01-21 16:54:37 +0100

    pairs

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -110,11 +110,9 @@ const MAINNET_ADRESSES = {
 export const PAIRS: Record<NetworkType, Pair[]> = {
   Devnet: [
     new Pair(USDC_DEV.address, USDT_DEV.address, FEE_TIERS[0]),
-    new Pair(USDC_DEV.address, SOL_DEV.address, FEE_TIERS[0]),
-    new Pair(MSOL_DEV.address, SOL_DEV.address, FEE_TIERS[0]),
-    new Pair(USDC_DEV.address, USDT_DEV.address, FEE_TIERS[1]),
-    new Pair(USDC_DEV.address, USDT_DEV.address, FEE_TIERS[2]),
-    new Pair(MSOL_DEV.address, WSOL_DEV.address, FEE_TIERS[0])
+    new Pair(USDC_DEV.address, WSOL_DEV.address, FEE_TIERS[0]),
+    new Pair(USDC_DEV.address, WSOL_DEV.address, FEE_TIERS[1]),
+    new Pair(USDC_DEV.address, WSOL_DEV.address, FEE_TIERS[2])
   ],
   Testnet: [],
   Mainnet: [
