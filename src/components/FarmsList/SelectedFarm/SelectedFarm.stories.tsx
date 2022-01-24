60772d842a27f668899a7a1eece0eb2104fec41c
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2022-01-24 15:51:01 +0100

    Remove unnecessary component in story

--- src/components/FarmsList/SelectedFarm/SelectedFarm.stories.tsx
+++ src/components/FarmsList/SelectedFarm/SelectedFarm.stories.tsx
@@ -20,21 +20,3 @@ storiesOf('farmsList/selectedFarm', module)
       </div>
     )
   })
-  .add('unstake', () => {
-    return (
-      <div style={{ width: 500, height: 500 }}>
-        <SelectedFarm
-          value={2345.34}
-          staked={233345}
-          pair={'xBTC - xUSD'}
-          currency={'SNY'}
-          currencyPrice={2}
-          apy={1}
-          liquidity={457}
-          stake={() => {}}
-          unstake={() => {}}
-          claimRewards={() => {}}
-        />
-      </div>
-    )
-  })
