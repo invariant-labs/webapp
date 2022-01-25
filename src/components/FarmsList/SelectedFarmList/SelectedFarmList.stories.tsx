382aa86b8f168c00e412e0153a83339a68f32527
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2022-01-25 12:35:21 +0100

    trunk fmt

--- src/components/FarmsList/SelectedFarmList/SelectedFarmList.stories.tsx
+++ src/components/FarmsList/SelectedFarmList/SelectedFarmList.stories.tsx
@@ -6,62 +6,62 @@ import SelectedFarmList from './SelectedFarmList'
 import icons from '@static/icons'
 
 storiesOf('farmsList/selectedFarm', module)
-    .addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>)
-    .add('list', () => {
-        return (
-            <Grid
-                style={{
-                    backgroundColor: '#1C1B1E',
-                    justifyContent: 'center',
-                    display: 'flex',
-                    paddingInline: 20,
-                    height: '100vh'
-                }}>
-                <SelectedFarmList
-                    title={'xBTC - xUSD'}
-                    rewards={'2 233.35 SNY'}
-                    iconTokenX={icons.BTC}
-                    iconTokenY={icons.USD}
-                    iconSNY={icons.SNY}
-                    data={[
-                        {
-                            value: 2345.34,
-                            staked: 233345,
-                            pair: 'xBTC - xUSD',
-                            currency: 'SNY',
-                            currencyPrice: 2,
-                            apy: 1,
-                            liquidity: 457,
-                            stake: () => { },
-                            unstake: () => { },
-                            claimRewards: () => { }
-                        },
-                        {
-                            value: 2345.34,
-                            staked: 233345,
-                            pair: 'xBTC - xUSD',
-                            currency: 'SNY',
-                            currencyPrice: 2,
-                            apy: 1,
-                            liquidity: 457,
-                            stake: () => { },
-                            unstake: () => { },
-                            claimRewards: () => { }
-                        },
-                        {
-                            value: 2345.34,
-                            staked: 233345,
-                            pair: 'xBTC - xUSD',
-                            currency: 'SNY',
-                            currencyPrice: 2,
-                            apy: 1,
-                            liquidity: 457,
-                            stake: () => { },
-                            unstake: () => { },
-                            claimRewards: () => { }
-                        }
-                    ]}
-                />
-            </Grid>
-        )
-    })
+  .addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>)
+  .add('list', () => {
+    return (
+      <Grid
+        style={{
+          backgroundColor: '#1C1B1E',
+          justifyContent: 'center',
+          display: 'flex',
+          paddingInline: 20,
+          height: '100vh'
+        }}>
+        <SelectedFarmList
+          title={'xBTC - xUSD'}
+          rewards={'2 233.35 SNY'}
+          iconTokenX={icons.BTC}
+          iconTokenY={icons.USD}
+          iconSNY={icons.SNY}
+          data={[
+            {
+              value: 2345.34,
+              staked: 233345,
+              pair: 'xBTC - xUSD',
+              currency: 'SNY',
+              currencyPrice: 2,
+              apy: 1,
+              liquidity: 457,
+              stake: () => {},
+              unstake: () => {},
+              claimRewards: () => {}
+            },
+            {
+              value: 2345.34,
+              staked: 233345,
+              pair: 'xBTC - xUSD',
+              currency: 'SNY',
+              currencyPrice: 2,
+              apy: 1,
+              liquidity: 457,
+              stake: () => {},
+              unstake: () => {},
+              claimRewards: () => {}
+            },
+            {
+              value: 2345.34,
+              staked: 233345,
+              pair: 'xBTC - xUSD',
+              currency: 'SNY',
+              currencyPrice: 2,
+              apy: 1,
+              liquidity: 457,
+              stake: () => {},
+              unstake: () => {},
+              claimRewards: () => {}
+            }
+          ]}
+        />
+      </Grid>
+    )
+  })
--- src/components/FarmsList/SelectedFarmList/SelectedFarmList.tsx
+++ src/components/FarmsList/SelectedFarmList/SelectedFarmList.tsx
@@ -4,79 +4,79 @@ import SelectedFarm, { ISelectedFarm } from '../SelectedFarm/SelectedFarm'
 import useStyle from './style'
 
 export interface ISelectedFarmList {
-    title: string
-    rewards: string
-    iconTokenX: string
-    iconTokenY: string
-    iconSNY: string
-    data: ISelectedFarm[]
+  title: string
+  rewards: string
+  iconTokenX: string
+  iconTokenY: string
+  iconSNY: string
+  data: ISelectedFarm[]
 }
 export const SelectedFarmList: React.FC<ISelectedFarmList> = ({
-    title,
-    data,
-    rewards,
-    iconTokenX,
-    iconTokenY,
-    iconSNY
+  title,
+  data,
+  rewards,
+  iconTokenX,
+  iconTokenY,
+  iconSNY
 }) => {
-    const classes = useStyle()
-    return (
-        <Grid className={classes.root}>
-            <Grid
-                className={classes.header}
-                container
-                direction='row'
-                justifyContent='flex-start'
-                alignItems='center'>
-                <img src={iconTokenX} alt={'Token in pool'} className={classes.bigIcon} />
-                <img src={iconTokenY} alt={'Token in pool'} className={classes.bigIcon} />
-                <Typography className={classes.header}>{title}</Typography>
-            </Grid>
-            <Grid
-                className={classes.header}
-                container
-                direction='row'
-                justifyContent='space-between'
-                alignItems='center'>
-                <Typography className={classes.postionsInfo}>
-                    Total Positions:
-                    <Typography display='inline' component='span' className={classes.value}>
-                        {data.length}
-                    </Typography>
-                </Typography>
-                <Typography
-                    className={classes.postionsInfo}
-                    style={{ display: 'flex', alignItems: 'center' }}>
-                    Reward:
-                    <Typography
-                        component='span'
-                        className={classes.value}
-                        style={{ display: 'flex', alignItems: 'center' }}>
-                        <img src={iconSNY} alt={'SNY token icon'} className={classes.smallIcon} /> {rewards} /
-                        day
-                    </Typography>
-                </Typography>
-            </Grid>
-            <Grid>
-                {data.map(element => (
-                    <div className={classes.tile}>
-                        <SelectedFarm
-                            value={element.value}
-                            staked={element.staked}
-                            pair={element.pair}
-                            currency={element.currency}
-                            currencyPrice={element.currencyPrice}
-                            apy={element.apy}
-                            liquidity={element.liquidity}
-                            stake={element.stake}
-                            unstake={element.unstake}
-                            claimRewards={element.claimRewards}
-                        />
-                    </div>
-                ))}
-            </Grid>
-        </Grid>
-    )
+  const classes = useStyle()
+  return (
+    <Grid className={classes.root}>
+      <Grid
+        className={classes.header}
+        container
+        direction='row'
+        justifyContent='flex-start'
+        alignItems='center'>
+        <img src={iconTokenX} alt={'Token in pool'} className={classes.bigIcon} />
+        <img src={iconTokenY} alt={'Token in pool'} className={classes.bigIcon} />
+        <Typography className={classes.header}>{title}</Typography>
+      </Grid>
+      <Grid
+        className={classes.header}
+        container
+        direction='row'
+        justifyContent='space-between'
+        alignItems='center'>
+        <Typography className={classes.postionsInfo}>
+          Total Positions:
+          <Typography display='inline' component='span' className={classes.value}>
+            {data.length}
+          </Typography>
+        </Typography>
+        <Typography
+          className={classes.postionsInfo}
+          style={{ display: 'flex', alignItems: 'center' }}>
+          Reward:
+          <Typography
+            component='span'
+            className={classes.value}
+            style={{ display: 'flex', alignItems: 'center' }}>
+            <img src={iconSNY} alt={'SNY token icon'} className={classes.smallIcon} /> {rewards} /
+            day
+          </Typography>
+        </Typography>
+      </Grid>
+      <Grid>
+        {data.map(element => (
+          <div className={classes.tile}>
+            <SelectedFarm
+              value={element.value}
+              staked={element.staked}
+              pair={element.pair}
+              currency={element.currency}
+              currencyPrice={element.currencyPrice}
+              apy={element.apy}
+              liquidity={element.liquidity}
+              stake={element.stake}
+              unstake={element.unstake}
+              claimRewards={element.claimRewards}
+            />
+          </div>
+        ))}
+      </Grid>
+    </Grid>
+  )
 }
 
 export default SelectedFarmList
