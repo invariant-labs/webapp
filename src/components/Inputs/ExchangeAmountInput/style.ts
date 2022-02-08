365fdebb8c8e2e93abf83a10436eed7737b8bad7
luckmer <woszczo04@gmail.com>
2022-02-08 09:38:49 +0100

    deleted spaces

--- src/components/Inputs/ExchangeAmountInput/style.ts
+++ src/components/Inputs/ExchangeAmountInput/style.ts
@@ -17,7 +17,6 @@ const useStyles = makeStyles(() => ({
     }
   },
 
-  
   maxButton: {
     fontSize: 12,
     color: colors.invariant.componentIn2,
@@ -27,7 +26,8 @@ const useStyles = makeStyles(() => ({
     borderRadius: 3,
     padding: '7px 0',
     backgroundColor: colors.invariant.light,
-    background: 'radial-gradient(118.38% 303.54% at 3.96% 118.38%, rgba(119, 72, 216, 0.1) 0%, rgba(119, 72, 216, 0) 100%), radial-gradient(57.34% 103.84% at 50% 0%, rgba(156, 231, 90, 0.1) 0%, rgba(156, 231, 90, 0) 100%)',
+    background:
+      'radial-gradient(118.38% 303.54% at 3.96% 118.38%, rgba(119, 72, 216, 0.1) 0%, rgba(119, 72, 216, 0) 100%), radial-gradient(57.34% 103.84% at 50% 0%, rgba(156, 231, 90, 0.1) 0%, rgba(156, 231, 90, 0) 100%)',
     '&:hover': {
       boxShadow: '0px 0px 20px -8px white'
     }
