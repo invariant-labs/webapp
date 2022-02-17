52730333afde03e6a8b7968aed6ac0419ca6729f
luckmer <woszczo04@gmail.com>
2022-02-17 12:34:08 +0100

    folder name

---
+++ src/containers/WrappedIdo/WrappedIDO.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import IDO from '@components/IDO/IDO'
+
+const WrappedIDO = () => {
+  return (
+    <>
+      <IDO
+        HEADER='Deposit your SOL'
+        xBTC='0.00000 xBTC'
+        xETH='0.00000 xETH'
+        SOL='0.0432 SOL'
+        USD='47.43 USD'
+        xUSD='46.643 xUSD'
+        ButtonHeader='Connect a wallet'
+      />
+    </>
+  )
+}
+
+export default WrappedIDO
