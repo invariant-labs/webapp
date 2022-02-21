8ea1d7b528aa4814d5a9d62a8dc7e9d0c5459e7b
luckmer <woszczo04@gmail.com>
2022-02-21 16:40:14 +0100

    added cursor pointer

--- src/components/Swap/style.ts
+++ src/components/Swap/style.ts
@@ -203,7 +203,8 @@ const useStyles = makeStyles((theme: Theme) => ({
   ableToHover: {
     border: `1px solid ${colors.invariant.light}`,
     borderRadius: '10px',
-    padding: '5px 15px 5px 15px'
+    padding: '5px 15px 5px 15px',
+    cursor: 'pointer'
   },
 
   transactionBottom: {
@@ -242,6 +243,56 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginLeft: 4,
     filter: 'brightness(0.8)',
     pointerEvents: 'none'
+  },
+
+  exchangeRateContainer: {
+    paddingTop: 20,
+    display: 'flex'
+  },
+
+  ExchangeFeeContainer: {
+    borderRight: '1px solid white',
+    marginRight: 15
+  },
+  feeContainer: {
+    marginRight: 15
+  },
+
+  feeHeader: {
+    ...typography.heading4,
+    color: colors.invariant.textGrey,
+    paddingBottom: 10,
+    display: 'flex',
+    flexDirection: 'row'
+  },
+
+  ExchangeHeader: {
+    ...typography.heading4,
+    color: colors.invariant.textGrey
+  },
+  percentHeader: {
+    ...typography.heading4,
+    color: colors.white.main,
+    '& span': {
+      paddingLeft: 5,
+      color: colors.invariant.textGrey
+    }
+  },
+  valueContainer: {
+    ...typography.heading4,
+    color: colors.white.main
+  },
+  swapIcon: {
+    width: 25,
+    height: 25,
+    backgroundSize: 'contain'
+  },
+  swapIconContainer: {
+    marginLeft: 20,
+    background: colors.invariant.newDark,
+    padding: '0 10px 0 10px',
+    borderRadius: 5,
+    cursor: 'pointer'
   }
 }))
 
