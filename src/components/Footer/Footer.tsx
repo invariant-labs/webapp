b8b627dab8597b9e8cb2e48630ed3d7edd859d8f
Adrian <adrianwojciak@interia.pl>
2022-04-28 22:22:27 +0200

    proper animation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import { Grid, Tooltip } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { Fade, Grid, Tooltip } from '@material-ui/core'
 import { social } from '@static/links'
 import icons from '@static/icons'
-import { Transition } from 'react-transition-group'
 import useStyles from './style'
 import { TransitionProps } from '@material-ui/core/transitions'
 
-export const FooterTransition: React.FC<TransitionProps & {
-  children?: React.ReactElement<any, any> | undefined;
-}> = ({ in: inProp, children, ...props }) => {
-  const duration = 300
-  const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0
+export const FooterTransition: React.FC<
+  TransitionProps & {
+    children?: React.ReactElement<any, any> | undefined
   }
+> = ({ children, ...props }) => {
+  const [translate, setTranslate] = useState('translateY(50px)')
 
-  const transitionStyles = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 },
-    unmounted: { opacity: 0 }
-  }
+  useEffect(() => {
+    setTranslate(props.in ? 'translateY(0)' : 'translateY(50px)')
+  }, [props.in])
 
   return (
-    <Transition in={inProp} {...(props as any)}>
-      {(state) => (
+    <Fade
+      {...props}
+      timeout={{
+        appear: 1000,
+        enter: 1000,
+        exit: 500
+      }}>
+      <div>
         <div
           style={{
-            ...defaultStyle,
-            ...transitionStyles[state]
-          }}>{children}</div>
-      )}
-    </Transition>
+            transition: 'transform 500ms',
+            transform: translate
+          }}>
+          {children}
+        </div>
+      </div>
+    </Fade>
   )
 }
 
@@ -41,35 +42,55 @@ export const Footer = () => {
   return (
     <Grid className={classes.footer}>
       <Grid className={classes.footerItem}>
-        <Tooltip classes={{ tooltip: classes.tooltip }} title='Github' placement='top' TransitionComponent={FooterTransition}>
+        <Tooltip
+          classes={{ tooltip: classes.tooltip }}
+          title='Github'
+          placement='top'
+          TransitionComponent={FooterTransition}>
           <a href={social.github} className={classes.footerLink} target='_blank'>
             <img className={classes.icon} src={icons.GithubIcon} alt={'github icon'} />
           </a>
         </Tooltip>
       </Grid>
       <Grid className={classes.footerItem}>
-        <Tooltip classes={{ tooltip: classes.tooltip }} title='Telegram' placement='top'>
+        <Tooltip
+          classes={{ tooltip: classes.tooltip }}
+          title='Telegram'
+          placement='top'
+          TransitionComponent={FooterTransition}>
           <a href={social.telegram} className={classes.footerLink} target='_blank'>
             <img className={classes.icon} src={icons.TelegramIcon} alt={'telegram icon'} />
           </a>
         </Tooltip>
       </Grid>
       <Grid className={classes.footerItem}>
-        <Tooltip classes={{ tooltip: classes.tooltip }} title='Twitter' placement='top'>
+        <Tooltip
+          classes={{ tooltip: classes.tooltip }}
+          title='Twitter'
+          placement='top'
+          TransitionComponent={FooterTransition}>
           <a href={social.twitter} className={classes.footerLink} target='_blank'>
             <img className={classes.icon} src={icons.TwitterIcon} alt={'twitter icon'} />
           </a>
         </Tooltip>
       </Grid>
       <Grid className={classes.footerItem}>
-        <Tooltip classes={{ tooltip: classes.tooltip }} title='Discord' placement='top'>
+        <Tooltip
+          classes={{ tooltip: classes.tooltip }}
+          title='Discord'
+          placement='top'
+          TransitionComponent={FooterTransition}>
           <a href={social.discord} className={classes.footerLink} target='_blank'>
             <img className={classes.icon} src={icons.DiscordIcon} alt={'discord icon'} />
           </a>
         </Tooltip>
       </Grid>
       <Grid className={classes.footerItem}>
-        <Tooltip classes={{ tooltip: classes.tooltip }} title='Medium' placement='top'>
+        <Tooltip
+          classes={{ tooltip: classes.tooltip }}
+          title='Medium'
+          placement='top'
+          TransitionComponent={FooterTransition}>
           <a href={social.medium} className={classes.footerLink} target='_blank'>
             <img className={classes.icon} src={icons.mediumIcon} alt={'medium icon'} />
           </a>
