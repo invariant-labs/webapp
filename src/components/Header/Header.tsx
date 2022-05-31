1062430a7d49de63f71353ff45baf9bf8012b5c3
Adrian <adrianwojciak99@gmail.com>
2022-05-31 16:09:59 +0200

    comments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -47,7 +47,7 @@ export const Header: React.FC<IHeader> = ({
 
   const routes =
     typeOfNetwork === NetworkType.MAINNET
-      ? ['swap', 'pool', 'stats', 'farms']
+      ? ['swap', 'pool', 'stats']
       : ['swap', 'pool', 'stats', 'farms', 'bonds']
 
   const otherRoutesToHighlight: Record<string, RegExp[]> = {
