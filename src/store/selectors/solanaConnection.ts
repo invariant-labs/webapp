4aff57d696d786d841850bc1049d6608e14aa553
Adrian <adrianwojciak@interia.pl>
2022-08-06 21:30:42 +0200

    lint

--- src/store/selectors/solanaConnection.ts
+++ src/store/selectors/solanaConnection.ts
@@ -3,7 +3,12 @@ import { keySelectors, AnyProps } from './helpers'
 
 const store = (s: AnyProps) => s[solanaConnectionSliceName] as ISolanaConnectionStore
 
-export const { network, status, slot, rpcAddress } = keySelectors(store, ['network', 'status', 'slot', 'rpcAddress'])
+export const { network, status, slot, rpcAddress } = keySelectors(store, [
+  'network',
+  'status',
+  'slot',
+  'rpcAddress'
+])
 
 export const solanaConnectionSelectors = { network, status, slot, rpcAddress }
 
