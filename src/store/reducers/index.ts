bcf01306bae4b62f27e62c91716e92e73983282c
Adrian <adrianwojciak@interia.pl>
2022-08-06 18:33:59 +0200

    migration

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import storage from 'redux-persist/lib/storage'
-import { persistReducer, createTransform } from 'redux-persist'
+import { persistReducer, createTransform, createMigrate, MigrationManifest } from 'redux-persist'
 import { NetworkType, SolanaNetworks } from '@consts/static'
 import { reducer as snackbarsReducer, snackbarsSliceName } from './snackbars'
 import { reducer as solanaWalletReducer, solanaWalletSliceName } from './solanaWallet'
-import { reducer as solanaConnectionReducer, solanaConnectionSliceName } from './solanaConnection'
+import {
+  ISolanaConnectionStore,
+  reducer as solanaConnectionReducer,
+  solanaConnectionSliceName
+} from './solanaConnection'
 import { poolsSliceName, reducer as poolsReducer } from './pools'
 import { swapSliceName, reducer as swapReducer } from './swap'
 import { positionsSliceName, reducer as positionsReducer } from './positions'
@@ -16,43 +20,54 @@ const transformNetwork = createTransform(
   (inboundState: any, _key) => {
     return inboundState
   },
-  (outboundState, key, fullState) => {
-    if (key === 'network') {
-      if (Object.values(NetworkType).includes(outboundState)) {
-        return outboundState
-      } else {
-        return NetworkType.MAINNET
-      }
+  (outboundState, key) => {
+    if (key === 'network' && !Object.values(NetworkType).includes(outboundState)) {
+      return NetworkType.MAINNET
     }
 
-    if (key === 'rpcAddress') {
-      if (typeof outboundState !== 'undefined') {
-        return outboundState
-      } else {
-        const network: NetworkType = Object.values(NetworkType).includes(fullState.network)
-          ? fullState.network
-          : NetworkType.MAINNET
-
-        switch (network) {
-          case NetworkType.DEVNET:
-            return SolanaNetworks.DEV
-          case NetworkType.TESTNET:
-            return SolanaNetworks.TEST
-          case NetworkType.LOCALNET:
-            return SolanaNetworks.LOCAL
-          case NetworkType.MAINNET:
-            return SolanaNetworks.MAIN_NIGHTLY
-        }
-      }
-    }
+    return outboundState
   }
 )
 
+const migrations: MigrationManifest = {
+  // @ts-expect-error
+  1: (state: ISolanaConnectionStore) => {
+    const network =
+      typeof state?.network !== 'undefined' && Object.values(NetworkType).includes(state.network)
+        ? state.network
+        : NetworkType.MAINNET
+
+    let rpcAddress
+
+    switch (network) {
+      case NetworkType.DEVNET:
+        rpcAddress = SolanaNetworks.DEV
+        break
+      case NetworkType.TESTNET:
+        rpcAddress = SolanaNetworks.TEST
+        break
+      case NetworkType.LOCALNET:
+        rpcAddress = SolanaNetworks.LOCAL
+        break
+      case NetworkType.MAINNET:
+        rpcAddress = SolanaNetworks.MAIN_NIGHTLY
+        break
+    }
+
+    return {
+      ...state,
+      rpcAddress
+    }
+  }
+}
+
 const connectionPersistConfig = {
   key: solanaConnectionSliceName,
+  version: 1,
   storage: storage,
   whitelist: ['network', 'rpcAddress'],
-  transforms: [transformNetwork]
+  transforms: [transformNetwork],
+  migrate: createMigrate(migrations, { debug: false })
 }
 
 const combinedReducers = combineReducers({
