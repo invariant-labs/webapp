1dc894e2bea3ee290d32e09713845c91ac904d8a
Adrian <adrianwojciak99@gmail.com>
2022-09-16 14:22:22 +0200

    done

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -354,6 +354,24 @@ export const minimumRangesForTiers = [20, 74, 80, 64, 28]
 
 export const maxSafeConcentrationsForTiers = [400.52, 41.49, 21.47, 8.13, 5.45]
 
-export const NEW_POSITION_FEE_TIERS = [FEE_TIERS[0], ...FEE_TIERS.slice(2)]
+export const getNewPositionFeeTiers = (
+  tokenAAddress: PublicKey | null,
+  tokenBAddress: PublicKey | null
+) => {
+  const USDC = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v')
+  const USDH = new PublicKey('USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX')
+
+  return [
+    FEE_TIERS[
+      tokenAAddress !== null &&
+      tokenBAddress !== null &&
+      ((tokenAAddress.equals(USDC) && tokenBAddress.equals(USDH)) ||
+        (tokenAAddress.equals(USDH) && tokenBAddress.equals(USDC)))
+        ? 1
+        : 0
+    ],
+    ...FEE_TIERS.slice(2)
+  ]
+}
 
 export { SolanaNetworks, DEFAULT_PUBLICKEY, MAX_U64, NetworkType }
