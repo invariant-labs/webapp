926d022c1d591a19094cbc2f8c3ec6011dab12ea
zielvna <zielvna@gmail.com>
2024-01-31 13:38:34 +0100

    remove console log

--- src/store/consts/utils.ts
+++ src/store/consts/utils.ts
@@ -1104,6 +1104,5 @@ export const getPoolsVolumeRanges = async (name: string): Promise<Record<string,
 const PRIORITY_FEE_DENOMINATOR = 9
 
 export const solToPriorityFee = (sol: number) => {
-  console.log(Math.round(sol * 5 * 10 ** PRIORITY_FEE_DENOMINATOR))
   return Math.round(sol * 5 * 10 ** PRIORITY_FEE_DENOMINATOR)
 }
