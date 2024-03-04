62092fe5fdb71ed5c07cfbe97e2f3c363e914845
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-03-04 10:25:32 +0100

    create a modal

--- src/components/NewPosition/JupiterIndexIndicator/JupiterIndexIndicator.tsx
+++ src/components/NewPosition/JupiterIndexIndicator/JupiterIndexIndicator.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react'
 import JupiterIcon from '@static/svg/JupiterIcon.svg'
+import closeWin from '@static/svg/closeWin.svg'
 import { jupiterIndicatorStyles } from './style.ts'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Modal from '@material-ui/core/Modal'
+import Grid from '@material-ui/core/Grid'
 
 interface IndicatorProps {
   marketId: string
@@ -19,6 +25,7 @@ interface ApiElementProps {
 export const JupiterIndexIndicator = ({ marketId }: IndicatorProps) => {
   const [isIndexed, setIsIndexed] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
+  const [open, setOpen] = useState<boolean>(false)
 
   const handleConn = async () => {
     try {
@@ -35,7 +42,12 @@ export const JupiterIndexIndicator = ({ marketId }: IndicatorProps) => {
       setError('Failed to fetch data. Please try again later.')
     }
   }
-
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
   useEffect(() => {
     let isMounted = true
 
@@ -50,16 +62,72 @@ export const JupiterIndexIndicator = ({ marketId }: IndicatorProps) => {
   return (
     <>
       {!error && isIndexed && (
-        <figure style={jupiterIndicatorStyles.container}>
-          <img
-            src={JupiterIcon}
-            style={jupiterIndicatorStyles.image}
-            aria-label='Indexed by Jupiter'
-            title='Jupiter Icon'
-            alt='Indexed by Jupiter'
-          />
-          <figcaption style={jupiterIndicatorStyles.caption}>Indexed by Jupiter</figcaption>
-        </figure>
+        <>
+          <Button onClick={handleOpen} variant='text'>
+            <figure style={jupiterIndicatorStyles.container}>
+              <img
+                src={JupiterIcon}
+                style={jupiterIndicatorStyles.image}
+                aria-label='Indexed by Jupiter'
+                title='Jupiter Icon'
+                alt='Indexed by Jupiter'
+              />
+              <figcaption style={jupiterIndicatorStyles.caption}>Indexed by Jupiter</figcaption>
+            </figure>
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby='modal-modal-title'
+            aria-describedby='modal-modal-description'>
+            <Box style={jupiterIndicatorStyles.modal} borderRadius='20px'>
+              <Grid container justifyContent='space-between'>
+                <Typography
+                  variant='body2'
+                  style={jupiterIndicatorStyles.headerColor}
+                  id='modal-modal-title'
+                  variant='h5'
+                  component='h2'>
+                  Jupiter indexing
+                </Typography>
+                <Button onClick={handleClose} aria-label='close'>
+                  <img src={closeWin} alt='Close modal' />
+                </Button>
+              </Grid>
+              <Typography
+                style={{
+                  ...jupiterIndicatorStyles.headerColor,
+                  marginTop: '10px',
+                  marginBottom: '10px'
+                }}
+                id='modal-modal-description'
+                variant='body2'>
+                Status: <span style={jupiterIndicatorStyles.detail}>Indexing active</span>
+              </Typography>
+              <Typography id='modal-modal-description' variant='body2'>
+                An indexed pool enhances your position's accessibility across the Solana ecosystem,
+                boosting profitability.
+                <br />
+                <br />
+                If a pool is not yet indexed, meeting three requirements will enable its indexing:
+                <ul>
+                  <li>
+                    Ensure your token exists on-chain with metadata following the{' '}
+                    <a style={jupiterIndicatorStyles.link} href='https://developers.metaplex.com/'>
+                      Metaplex Token Metadata
+                    </a>
+                  </li>
+                  <li>Maintain at least $250 liquidity on both buy and sell sides.</li>
+                  <li>
+                    Limit buy and sell price impact to 30% to prevent single-sided liquidity markets
+                  </li>
+                </ul>
+                Once these criteria are met, Jupiter automatically lists your token within minutes
+                (usually up to ~30 min).
+              </Typography>
+            </Box>
+          </Modal>
+        </>
       )}
     </>
   )
--- src/components/NewPosition/JupiterIndexIndicator/style.ts
+++ src/components/NewPosition/JupiterIndexIndicator/style.ts
@@ -3,7 +3,7 @@ export const jupiterIndicatorStyles = {
   container: {
     position: 'absolute',
     height: 32,
-    top: -5,
+    top: -10,
     zIndex: 999,
     right: 5,
     padding: 0,
@@ -11,10 +11,33 @@ export const jupiterIndicatorStyles = {
   },
   image: {
     height: 32,
+    width: 32,
     filter: 'drop-shadow(0px 0px 26px #bfef8c)',
     animation: 'pulseAnimation 1s infinite'
   },
   caption: {
     fontSize: 0
+  },
+  modal: {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 312,
+    backgroundColor: '#202946',
+    color: '#A9B6BF',
+    padding: '10px 20px'
+  },
+  detail: {
+    color: '#2EE09A'
+  },
+  link: {
+    color: '#EF84F5'
+  },
+  headerColor: {
+    color: '#fff'
+  },
+  button: {
+    minWidth: 'none'
   }
 }
