1fe9294f9f0c7326548ec83ea53c3e45ff11d462
Przemyslaw Kulej <przemek.kulej1@gmail.com>
2024-10-26 15:51:33 +0200

    Updated telegram link

--- src/static/links.ts
+++ src/static/links.ts
@@ -2,7 +2,8 @@ export const social = {
   twitter: 'https://twitter.com/invariant_labs',
   github: 'https://github.com/invariant-labs',
   discord: 'https://discord.gg/w6hTeWTJvG',
-  telegram: 'https://t.me/+-C9-e6L08AY4YWI0',
+  telegram: 'https://t.me/Invariant_app',
   medium: 'https://medium.com/@invariant_labs',
   docs: 'https://docs.invariant.app'
 }
+  
