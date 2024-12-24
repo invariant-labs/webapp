d5b494059dfe84e6297cece5763b6f7f4c0c0f2f
none00y <mich_smyk@wp.pl>
2024-12-24 13:37:04 +0100

    fix linting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -781,7 +781,11 @@ export const handleSimulate = async (
 
         okChanges += 1
       } else if (
-        byAmountIn ? allFailedData.amountOut.lt(result) : allFailedData.amountOut.eq(MAX_U64) ? true : allFailedData.amountOut.lt(result)
+        byAmountIn
+          ? allFailedData.amountOut.lt(result)
+          : allFailedData.amountOut.eq(MAX_U64)
+            ? true
+            : allFailedData.amountOut.lt(result)
       ) {
         allFailedData = {
           amountOut: result,
