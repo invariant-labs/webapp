a4a3b792fef2429962c4f93f53a0223e60b7eb52
Piotr <piotr.matlak.contact@gmail.com>
2025-02-24 08:16:41 +0100

    font weight 600 to all tables headers

--- src/components/Stats/TokenListItem/style.ts
+++ src/components/Stats/TokenListItem/style.ts
@@ -61,16 +61,16 @@ export const useStyles = makeStyles()((theme: Theme) => ({
   },
 
   header: {
-    '& p': {
+    '& p.MuiTypography-root': {
       ...typography.heading4,
-      fontWeight: 400,
+      fontWeight: 600,
       display: 'flex',
       justifyContent: 'start',
-      alignItems: 'center'
-    },
-    [theme.breakpoints.down('sm')]: {
-      '& p': {
-        ...typography.caption2
+      alignItems: 'center',
+
+      [theme.breakpoints.down('sm')]: {
+        ...typography.caption2,
+        fontWeight: 600
       }
     }
   },
