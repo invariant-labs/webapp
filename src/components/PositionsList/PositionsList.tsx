bc64788cf9b25c19b8337cc5d45c5a844af76adb
kuba80-02 <kuba8369@gmail.com>
2025-03-10 08:39:38 +0100

    Fix redundant prop

--- src/components/PositionsList/PositionsList.tsx
+++ src/components/PositionsList/PositionsList.tsx
@@ -39,7 +39,6 @@ export const PositionsList: React.FC<IProps> = ({
   noConnectedBlockerProps,
   handleRefresh,
   currentNetwork,
-  currentNetwork,
   // pageChanged,
   // length,
   // lockedLength,
@@ -155,14 +154,14 @@ export const PositionsList: React.FC<IProps> = ({
 
             <Grid className={classes.searchWrapper}>
               <Grid className={classes.filtersContainer}>
-                {/* <FilterSearch
+                <FilterSearch
                   loading={loading}
                   bp='md'
                   networkType={currentNetwork}
                   filtersAmount={2}
                   selectedFilters={selectedFilters}
                   setSelectedFilters={setSelectedFilters}
-                /> */}
+                />
               </Grid>
 
               <Grid
@@ -228,14 +227,14 @@ export const PositionsList: React.FC<IProps> = ({
                 </Grid>
               </Grid>
 
-              {/* <FilterSearch
+              <FilterSearch
                 bp='md'
                 loading={loading}
                 networkType={currentNetwork}
                 filtersAmount={2}
                 selectedFilters={selectedFilters}
                 setSelectedFilters={setSelectedFilters}
-              /> */}
+              />
             </Grid>
           </Grid>
         </Grid>
