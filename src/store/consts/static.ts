5c98c6054dbf3c306ee74a77df792863c862ca19
Jakub <jakubkowolik00@gmail.com>
2025-04-10 10:42:44 +0200

    names revert

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -48,7 +48,7 @@ export const MSOL_DEV: Token = {
     'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So/logo.png'
 }
 export const BTC_DEV: Token = {
-  symbol: 'BTCAAAAAAAAAAA',
+  symbol: 'BTC',
   address: new PublicKey(MOCK_TOKENS.BTC),
   decimals: 6,
   name: 'Wrapped Bitcoin (Sollet)',
@@ -56,7 +56,7 @@ export const BTC_DEV: Token = {
     'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E/logo.png'
 }
 export const WSOL_DEV: Token = {
-  symbol: 'WSOLASDASD',
+  symbol: 'WSOL',
   address: new PublicKey(MOCK_TOKENS.WSOL),
   decimals: 9,
   name: 'Wrapped Solana',
