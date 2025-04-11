19527df470a54cced10ff583acdf6f53406a66f2
Piotr <piotr.matlak.contact@gmail.com>
2025-04-11 10:02:07 +0200

    remove background from assets placeholder

--- src/components/OverviewYourPositions/components/YourWallet/styles.ts
+++ src/components/OverviewYourPositions/components/YourWallet/styles.ts
@@ -281,8 +281,13 @@ export const useStyles = makeStyles<{ isLoading: boolean; isScrollHide: boolean 
       padding: '32px',
       gap: '16px',
       border: 'none',
-      background:
-        'linear-gradient(360deg, rgba(32, 41, 70, 0.8) 0%, rgba(17, 25, 49, 0.8) 100%), linear-gradient(180deg, #010514 0%, rgba(1, 5, 20, 0) 100%)'
+      background: colors.invariant.component,
+      borderRadius: 24,
+      [theme.breakpoints.up('lg')]: {
+        background:
+          'linear-gradient(360deg, rgba(32, 41, 70, 0.8) 0%, rgba(17, 25, 49, 0.8) 100%), linear-gradient(180deg, #010514 0%, rgba(1, 5, 20, 0) 100%)',
+        borderRadius: 0
+      }
     },
     emptyStateText: {
       ...typography.heading2,
