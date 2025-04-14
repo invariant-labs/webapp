04509fbc4e79a106f983ac3d070814ea78a63a83
Piotr <piotr.matlak.contact@gmail.com>
2025-04-14 12:41:05 +0200

    add prices values

--- src/components/PositionDetails/SinglePositionPlot/SinglePositionPlot.tsx
+++ src/components/PositionDetails/SinglePositionPlot/SinglePositionPlot.tsx
@@ -4,6 +4,7 @@ import {
   calcPriceByTickIndex,
   calcTicksAmountInRange,
   calculateConcentration,
+  formatNumberWithoutSuffix,
   formatNumberWithSuffix,
   numberToString,
   spacingMultiplicityGte,
@@ -11,7 +12,7 @@ import {
   truncateString
 } from '@utils/utils'
 import { PlotTickData } from '@store/reducers/positions'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import useStyles from './style'
 import { getMinTick } from '@invariant-labs/sdk/lib/utils'
@@ -159,10 +160,72 @@ const SinglePositionPlot: React.FC<ISinglePositionPlot> = ({
   const maxPercentage = (max / currentPrice - 1) * 100
   const concentration = calculateConcentration(leftRange.index, rightRange.index)
 
+  const buyPercentageDifference = useMemo(() => {
+    if (
+      tokenAPriceData?.buyPrice === undefined ||
+      globalPrice === undefined ||
+      tokenBPriceData?.price === undefined
+    ) {
+      return
+    }
+    return ((tokenAPriceData.buyPrice / tokenBPriceData?.price - globalPrice) / globalPrice) * 100
+  }, [tokenAPriceData?.buyPrice, globalPrice, tokenBPriceData?.price])
+
+  const sellPercentageDifference = useMemo(() => {
+    if (
+      tokenAPriceData?.sellPrice === undefined ||
+      globalPrice === undefined ||
+      tokenBPriceData?.price === undefined
+    ) {
+      return
+    }
+    return ((tokenAPriceData.sellPrice / tokenBPriceData?.price - globalPrice) / globalPrice) * 100
+  }, [tokenAPriceData?.sellPrice, globalPrice, tokenBPriceData?.price])
+
   return (
     <Box className={classes.container}>
       <Box className={classes.headerContainer}>
-        <Typography className={classes.header}>Price range</Typography>
+        <Grid>
+          <Typography className={classes.header}>Price range</Typography>
+          {
+            <>
+              <div className={classes.priceBlock}>
+                <Typography className={classes.currentPrice}>
+                  {formatNumberWithoutSuffix(midPrice.x)} {tokenX.name}/{tokenY.name}
+                </Typography>
+              </div>
+              <div className={classes.priceBlock}>
+                {globalPrice && (
+                  <Typography
+                    className={classes.currentPrice}
+                    style={{ color: colors.invariant.blue }}>
+                    {formatNumberWithoutSuffix(globalPrice)} {tokenX.name}/{tokenY.name}
+                  </Typography>
+                )}
+              </div>
+              <div className={classes.priceBlock}>
+                {buyPercentageDifference && (
+                  <Typography
+                    className={classes.currentPrice}
+                    style={{ color: colors.invariant.plotGreen }}>
+                    {buyPercentageDifference < 0 ? '-' : '+'}
+                    {formatNumberWithoutSuffix(Math.abs(buyPercentageDifference))}%
+                  </Typography>
+                )}
+              </div>
+              <div className={classes.priceBlock}>
+                {sellPercentageDifference && (
+                  <Typography
+                    className={classes.currentPrice}
+                    style={{ color: colors.invariant.plotRed }}>
+                    {sellPercentageDifference < 0 ? '-' : '+'}{' '}
+                    {formatNumberWithoutSuffix(Math.abs(sellPercentageDifference))}%
+                  </Typography>
+                )}
+              </div>
+            </>
+          }
+        </Grid>
         <Grid>
           <RangeIndicator inRange={min <= currentPrice && currentPrice <= max} />
           <Grid gap={'2px'} mt={1} display='flex' flexDirection='column' alignItems='flex-end'>
