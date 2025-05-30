181b51c6cc865468d4f3e9196ea7902320cd3241
Piotr <piotr.matlak.contact@gmail.com>
2025-05-30 12:27:58 +0200

    refactor pagination and align tooltip

--- src/common/TooltipHover/TooltipHover.tsx
+++ src/common/TooltipHover/TooltipHover.tsx
@@ -1,7 +1,7 @@
 import { Box, Tooltip, TooltipProps, useMediaQuery } from '@mui/material'
 import useStyles from './style'
 import { TooltipTransition } from './TooltipTransition/TooltipTransition'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { theme } from '@static/theme'
 import useIsMobile from '@store/hooks/isMobile'
 
@@ -38,7 +38,9 @@ export const TooltipHover = ({
   const [open, setOpen] = useState(false)
   const [childrenHover, setChildrenHover] = useState(false)
   const [titleHover, setTitleHover] = useState(false)
-  const [callback, setCallback] = useState<NodeJS.Timeout | null>(null)
+
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
   const isClosingOnScroll = useMediaQuery(theme.breakpoints.down(1200))
   const isMobile = useIsMobile()
 
@@ -56,37 +58,35 @@ export const TooltipHover = ({
   }, [isClosingOnScroll])
 
   useEffect(() => {
-    if (isMobile) {
-      return
-    }
+    if (isMobile) return
+
     if (titleHover || childrenHover) {
-      if (callback) clearTimeout(callback)
-      setOpen(true)
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
+      if (!open) setOpen(true)
     } else {
-      const timeout = setTimeout(() => {
+      timeoutRef.current = setTimeout(() => {
         setOpen(false)
       }, 100)
-      setCallback(timeout)
     }
 
     return () => {
-      if (callback) clearTimeout(callback)
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
     }
-  }, [titleHover, childrenHover])
+  }, [titleHover, childrenHover, isMobile, open])
 
   useEffect(() => {
     if (isMobile && open) {
-      if (callback) clearTimeout(callback)
-      const timeout = setTimeout(() => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
+      timeoutRef.current = setTimeout(() => {
         setOpen(false)
       }, 2000)
-      setCallback(timeout)
     }
 
     return () => {
-      if (callback) clearTimeout(callback)
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
     }
   }, [open, isMobile])
+
   useEffect(() => {
     if (!allowEnterTooltip) {
       setOpen(false)
@@ -94,6 +94,7 @@ export const TooltipHover = ({
   }, [allowEnterTooltip])
 
   if (!title) return children
+
   return (
     <Tooltip
       classes={{ tooltip: gradient ? classes.tooltipGradient : classes.tooltipNoGradient }}
@@ -120,17 +121,24 @@ export const TooltipHover = ({
             setOpen(true)
           }
         }}
-        onMouseEnter={
-          allowEnterTooltip
-            ? () => {
-                setChildrenHover(true)
-                setOpen(true)
-              }
-            : undefined
-        }
+        onMouseEnter={() => {
+          if (allowEnterTooltip && !isMobile) {
+            setChildrenHover(true)
+            setOpen(true)
+          }
+        }}
+        onMouseDown={() => {
+          if (allowEnterTooltip && isMobile) {
+            setChildrenHover(true)
+            setOpen(true)
+          }
+        }}
         onMouseLeave={() => setChildrenHover(false)}
         onFocus={allowEnterTooltip ? () => setChildrenHover(true) : undefined}
-        onBlur={() => setChildrenHover(false)}>
+        onBlur={() => {
+          setChildrenHover(false)
+          setOpen(false)
+        }}>
         {children}
       </span>
     </Tooltip>
