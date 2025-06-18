7a1e6eb2bd4b5bbe606512dd1e74ac3ec8fc2690
zielvna <zielvna@gmail.com>
2025-06-18 10:42:07 +0200

    add gap to transaction details box

--- src/components/Swap/style.ts
+++ src/components/Swap/style.ts
@@ -381,6 +381,7 @@ export const useStyles = makeStyles()((theme: Theme) => ({
     width: '100%',
     justifyContent: 'space-between',
     flexDirection: 'row',
+    marginBottom: 12,
     [theme.breakpoints.down('sm')]: {
       flexDirection: 'column-reverse',
       gap: 4
