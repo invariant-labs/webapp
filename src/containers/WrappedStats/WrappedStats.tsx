b58240989b102e276f2050b2cdbb8a40f175be67
zielvna <zielvna@gmail.com>
2025-06-18 15:17:29 +0200

    refactor stats overview

--- src/containers/WrappedStats/WrappedStats.tsx
+++ src/containers/WrappedStats/WrappedStats.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import useStyles from './styles'
-import { Box, Grid, Typography, useMediaQuery } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { EmptyPlaceholder } from '@common/EmptyPlaceholder/EmptyPlaceholder'
 import {
+  cumulativeFees,
+  cumulativeVolume,
   currentInterval,
   fees,
   isLoading,
@@ -19,23 +21,16 @@ import {
 import { network } from '@store/selectors/solanaConnection'
 import { actions } from '@store/reducers/stats'
 import { actions as snackbarActions } from '@store/reducers/snackbars'
-import Volume from '@components/Stats/Volume/Volume'
-import Liquidity from '@components/Stats/Liquidity/Liquidity'
-import VolumeBar from '@components/Stats/volumeBar/VolumeBar'
 import TokensList from '@components/Stats/TokensList/TokensList'
 import PoolList from '@components/Stats/PoolList/PoolList'
 import { VariantType } from 'notistack'
 import { FilterSearch, ISearchToken } from '@common/FilterSearch/FilterSearch'
 import { unknownTokenIcon } from '@static/icons'
-import { Separator } from '@common/Separator/Separator'
-import { colors, theme } from '@static/theme'
 import { Intervals as IntervalsKeys } from '@store/consts/static'
-import Intervals from '@components/Stats/Intervals/Intervals'
+import Overview from '@components/Stats/Overview/Overview'
 
 export const WrappedStats: React.FC = () => {
-  const { classes, cx } = useStyles()
-
-  const isSm = useMediaQuery(theme.breakpoints.down('sm'))
+  const { classes } = useStyles()
 
   const dispatch = useDispatch()
 
@@ -49,6 +44,8 @@ export const WrappedStats: React.FC = () => {
   const liquidityPlotData = useSelector(liquidityPlot)
   const isLoadingStats = useSelector(isLoading)
   const currentNetwork = useSelector(network)
+  const cumulativeVolumeData = useSelector(cumulativeVolume)
+  const cumulativeFeesData = useSelector(cumulativeFees)
 
   const lastUsedInterval = useSelector(currentInterval)
   const lastFetchedInterval = useSelector(lastInterval)
@@ -138,57 +135,19 @@ export const WrappedStats: React.FC = () => {
         </Grid>
       ) : (
         <>
-          <Box display='flex' justifyContent='space-between' alignItems='center' mb={4}>
-            <Typography className={classes.subheader}>Overview</Typography>
-            <Intervals
-              interval={lastUsedInterval ?? IntervalsKeys.Daily}
-              setInterval={updateInterval}
-            />
-          </Box>
-          <Grid
-            container
-            className={cx(classes.plotsRow, {
-              [classes.loadingOverlay]: isLoadingStats
-            })}>
-            <Box display='flex' gap={'24px'} flexDirection={isSm ? 'column' : 'row'} width='100%'>
-              <Volume
-                volume={volumeInterval.value}
-                data={volumePlotData}
-                className={classes.plot}
-                isLoading={isLoadingStats}
-                lastStatsTimestamp={lastStatsTimestamp}
-                interval={lastUsedInterval ?? IntervalsKeys.Daily}
-              />
-              {
-                <Separator
-                  color={colors.invariant.light}
-                  margin={isSm ? '0 24px' : '24px 0'}
-                  width={1}
-                  isHorizontal={isSm}
-                />
-              }
-              <Liquidity
-                liquidityVolume={tvlInterval.value}
-                data={liquidityPlotData}
-                className={classes.plot}
-                isLoading={isLoadingStats}
-                lastStatsTimestamp={lastStatsTimestamp}
-                interval={lastUsedInterval ?? IntervalsKeys.Daily}
-              />
-            </Box>
-          </Grid>
-          <Grid className={classes.row}>
-            <VolumeBar
-              volume={volumeInterval.value}
-              percentVolume={volumeInterval.change}
-              tvlVolume={tvlInterval.value}
-              percentTvl={tvlInterval.change}
-              feesVolume={feesInterval.value}
-              percentFees={feesInterval.change}
-              isLoading={isLoadingStats}
-              interval={lastUsedInterval ?? IntervalsKeys.Daily}
-            />
-          </Grid>
+          <Overview
+            feesInterval={feesInterval}
+            isLoadingStats={isLoadingStats}
+            lastStatsTimestamp={lastStatsTimestamp}
+            lastUsedInterval={lastUsedInterval}
+            updateInterval={updateInterval}
+            volumeInterval={volumeInterval}
+            volumePlotData={volumePlotData}
+            liquidityPlotData={liquidityPlotData}
+            tvlInterval={tvlInterval}
+            cumulativeVolume={cumulativeVolumeData}
+            cumulativeFees={cumulativeFeesData}
+          />
           <Grid className={classes.rowContainer}>
             <Typography className={classes.subheader} mb={2}>
               Top pools
